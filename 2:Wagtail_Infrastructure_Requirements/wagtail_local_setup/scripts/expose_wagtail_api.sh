adb9f28161dacbed6a8ca7818cdfacbb27fed1bd
Liam Miller <liam.miller@madetech.com>
2021-04-28 15:05:29 +0100

    fixing path issue - Spike 2, local-setup.

--- 2:Wagtail_Infrastructure_Requirements/wagtail_local_setup/scripts/expose_wagtail_api.sh
+++ 2:Wagtail_Infrastructure_Requirements/wagtail_local_setup/scripts/expose_wagtail_api.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-WAGTAIL_APP_ROUTE="../app/app/"
+WAGTAIL_APP_ROUTE="app/app/"
 WAGTAIL_API_FILE="api.py"
 WAGTAIL_URL_FILE="urls.py"
 WAGTAIL_SETTINGS_FILE="settings/base.py"
 
 sed -i -E "/^INSTALLED_APPS =/a'wagtail.api.v2','rest_framework'," $WAGTAIL_APP_ROUTE$WAGTAIL_SETTINGS_FILE
 
-cp ./supportingPythonFiles/$WAGTAIL_API_FILE $WAGTAIL_APP_ROUTE$WAGTAIL_API_FILE
-cp ./supportingPythonFiles/$WAGTAIL_URL_FILE $WAGTAIL_APP_ROUTE$WAGTAIL_URL_FILE
+cp ./scripts/supportingPythonFiles/$WAGTAIL_API_FILE $WAGTAIL_APP_ROUTE$WAGTAIL_API_FILE
+cp ./scripts/supportingPythonFiles/$WAGTAIL_URL_FILE $WAGTAIL_APP_ROUTE$WAGTAIL_URL_FILE
