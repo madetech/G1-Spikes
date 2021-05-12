a1ca7243c556ad1ee54f9666bdba51c130c928c7
Liam Miller <liam.miller@madetech.com>
2021-05-12 11:29:27 +0100

    Adding comment regarding key.

--- 5:Wagtail_Query_Extension/test-code/testSite/testSite/settings/dev.py
+++ 5:Wagtail_Query_Extension/test-code/testSite/testSite/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f-bt9p=$%xxq21gm3$z_oonf4*ngl7b7z01o98hqdq5w4=o8kr'
+SECRET_KEY = 'f-bt9p=$%xxq21gm3$z_oonf4*ngl7b7z01o98hqdq5w4=o8kr' # This is a test key created on set-up, no risk in being exposed.
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
